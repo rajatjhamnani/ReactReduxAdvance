3a97c135a2b6a6a3847d1988b7057799456812c7
rajatjhamnani <rajatjhamnani@gmail.com>
2024-02-10 02:09:33 +0530

    Doing Network calls and andling response the right way

--- src/App.js
+++ src/App.js
@@ -1,16 +1,77 @@
-import { useSelector } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import Cart from "./components/Cart/Cart";
 import Layout from "./components/Layout/Layout";
 import Products from "./components/Shop/Products";
 
+import { useEffect } from "react";
+import { showNotification } from "./components/store/ShowCart";
+import Notification from "./components/UI/Notification";
+let isInitial = true;
 function App() {
   const cartIsShown = useSelector((state) => state.toggle.showCart);
+  const cart = useSelector((state) => state.cartData);
+  const notification = useSelector((state) => state.toggle.notification);
+  const dispatch = useDispatch();
+  useEffect(() => {
+    const sendCartData = async () => {
+      dispatch(
+        showNotification({
+          status: "pending",
+          title: "sending...",
+          message: "sending cart data",
+        })
+      );
+      const response = await fetch(
+        "https://expense-tracker-87bd8-default-rtdb.firebaseio.com/cartData.json",
+        {
+          method: "PUT",
+          body: JSON.stringify(cart),
+          headers: {
+            "content-type": "application/json",
+          },
+        }
+      );
+      if (!response.ok) {
+        throw new Error("sending cart data failed");
+      }
+
+      dispatch(
+        showNotification({
+          status: "success",
+          title: "success",
+          message: "sent cart data successfully",
+        })
+      );
+    };
+    if (isInitial) {
+      isInitial = false;
+      return;
+    }
+    sendCartData().catch((error) => {
+      dispatch(
+        showNotification({
+          status: "error",
+          title: "Error",
+          message: "sending cart data failed",
+        })
+      );
+    });
+  }, [cart, dispatch]);
   console.log(cartIsShown);
   return (
-    <Layout>
-      {cartIsShown && <Cart />}
-      <Products />
-    </Layout>
+    <>
+      {notification && (
+        <Notification
+          status={notification.status}
+          title={notification.title}
+          message={notification.message}
+        />
+      )}
+      <Layout>
+        {cartIsShown && <Cart />}
+        <Products />
+      </Layout>
+    </>
   );
 }
 
--- src/components/store/ShowCart.js
+++ src/components/store/ShowCart.js
@@ -2,6 +2,7 @@ import { createSlice } from "@reduxjs/toolkit";
 
 const initialCartState = {
   showCart: false,
+  notification: null,
 };
 
 export const cartSlice = createSlice({
@@ -11,8 +12,15 @@ export const cartSlice = createSlice({
     hideCart: (state, action) => {
       state.showCart = !state.showCart;
     },
+    showNotification: (state, action) => {
+      state.notification = {
+        status: action.payload.status,
+        title: action.payload.title,
+        message: action.payload.message,
+      };
+    },
   },
 });
 
-export const { hideCart } = cartSlice.actions;
+export const { hideCart, showNotification } = cartSlice.actions;
 export default cartSlice.reducer;
